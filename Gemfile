f6d9aab81a43f8e34a26266425ea35b198c128de
Marcin Zgórecki <marcin.zgorecki@chargify.com>
2024-01-04 11:56:46 +0100

    Add should-matchers

--- Gemfile
+++ Gemfile
@@ -52,8 +52,9 @@ gem "pagy", "~> 6.2"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
-  gem "rspec-rails", "~> 6.1"
   gem "factory_bot_rails", "~> 6.4"
+  gem "rspec-rails", "~> 6.1"
+  gem "shoulda-matchers", "~> 6.0"
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,12 +29,15 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+require "factory_bot_rails"
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
-
+  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -62,4 +65,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
