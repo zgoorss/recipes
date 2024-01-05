79bb54468c102f3c09407f57623c9cd843b966b6
Marcin Zgórecki <marcin.zgorecki@chargify.com>
2024-01-05 16:41:56 +0100

    Update migration

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,21 +5,10 @@ class Recipe < ApplicationRecord
 
   validates :title, :cook_time, :prep_time, :ingredients, presence: true
 
-  after_commit :update_ingredients_search
-
   pg_search_scope :by_ingredients, against: :ingredients, using: {
     tsearch: {
       dictionary: "english",
       tsvector_column: "ingredients_search"
     }
   }
-
-  private
-
-  def update_ingredients_search
-    self
-      .class
-      .where(ingredients_search: nil)
-      .update_all("ingredients_search = to_tsvector(array_to_string(ingredients, ','))")
-  end
 end
